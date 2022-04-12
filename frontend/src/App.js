79781574117a9de101db241a02302f436b6b7f60
Niko Suoniemi <niko.suoniemi@tuni.fi>
2022-04-12 16:28:22 +0300

    Implement adding marker and post.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,37 +2,57 @@ import './App.css';
 import React from 'react';
 import axios from 'axios';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+const path = '/locations';
 
 const App = () => {
-  const [locations, setLocations] = React.useState([]);
+    const [locations, setLocations] = React.useState([]);
 
-  // GET locations from backend to state object at component mount
-  React.useEffect(() => {
-    (async () => {
-        let resp = await axios.get('/locations');
-        setLocations(resp.data);
-    })();
-  }, []);
+    // GET locations from backend to state array at component mount
+    React.useEffect(() => {
+        (async () => {
+            let resp = await axios.get(path);
+            setLocations(resp.data);
+        })();
+    }, []);
 
-  // Map locations to leaflet Markers
-  let markers = locations.map(loc =>
-      <Marker position={[loc.latitude, loc.longitude]}>
-        <Popup>
-          Id: {loc.id}<br />
-          Latitude: {loc.latitude}<br />
-          Longitude: {loc.longitude}
-        </Popup>
-      </Marker>
-  );
+    // POST to backend and add new location to state array
+    const post = async (loc) => {
+        let resp = await axios.post(path, {
+            latitude: loc.lat,
+            longitude: loc.lng,
+        });
+        setLocations((prevState) => [...prevState, resp.data]);
+    };
 
-  return (
-    <MapContainer center={[51.505, -0.09]} zoom={2} scrollWheelZoom={false}>
-      <TileLayer
-        attribution="&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"
-        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-      />
-      {markers}
-    </MapContainer>
-  );
-}
+    // Map locations to leaflet Markers
+    let markers = locations.map((loc) => (
+        <Marker position={[loc.latitude, loc.longitude]}>
+            <Popup>
+                Id: {loc.id}
+                <br />
+                Latitude: {loc.latitude}
+                <br />
+                Longitude: {loc.longitude}
+            </Popup>
+        </Marker>
+    ));
+
+    return (
+        <MapContainer
+            center={[51.505, -0.09]}
+            zoom={2}
+            scrollWheelZoom={false}
+            doubleClickZoom={false}
+            whenCreated={(map) => {
+                map.on('dblclick', (e) => post(e.latlng));
+            }}
+        >
+            <TileLayer
+                attribution="&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {markers}
+        </MapContainer>
+    );
+};
 export default App;
